bdaf673010b7b5299c36e68831d20e7e832f4294
asifs-cse <asifs.cse@gmail.com>
2022-03-11 21:40:15 +0530

    first problem

---
+++ secound.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,n;
+    cin>>a>>b;
+    // for (int i = 0; i < n; i++)
+    // {
+        
+    // }
+    int j=0;
+    while(j<10){
+        cout<<j<<" ";
+        j++;
+        if(j==10) break;
+    }
+    
+}
