575b0a9ba4aecb2587368454a27f4a5e77db063f
asifs-cse <asifs.cse@gmail.com>
2022-03-12 10:41:43 +0530

    placement

--- placemonet.cpp
+++ placemonet.cpp
@@ -5,16 +5,10 @@ int main(){
     string branch;
     int year, back_log;
     double per;
-    cout<<"Enter your year\n";
-    cin>>year;
-    cout<<"Enter your back log: \n";
-    cin>>back_log;
-    cout<<"Enter your branch: \n";
-    cin>>branch;
-    cout<<"Entr your persentange: \n";
-    cin>>per;
+    cout<<"Enter your year, back logk, branch and persentage: ";
+    cin>>year>>back_log>>branch>>per;
 
-    if(year == 3 and back_log ==0 and branch == "CSE" or branch =="IOT" and per>=70){
+    if(year == 3 and back_log ==0 and branch == "CSE" or branch =="IOT" or branch=="ECE" and per>=70){
         cout<<"Your are eligable";
     }else{
         cout<<"You are not eligable";
