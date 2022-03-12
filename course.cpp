ec0fb84124897d3b8905d93b14b508b2b387b685
asifs-cse <asifs.cse@gmail.com>
2022-03-12 10:49:58 +0530

    sourse

---
+++ course.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int bc, fees;
+
+    cout<<"If you are become coder student, press 1";
+    cin>>bc;
+
+    if(bc==1){
+        cout<<"Your fees is 3000";
+    }else{
+        cout<<"Your fees is 5000";
+    }
+
+    return 0;
+}
