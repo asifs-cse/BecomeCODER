96a648a73b703de79ade492e1a8654e088515343
asifs-cse <asifs.cse@gmail.com>
2022-03-17 14:15:21 +0530

    fact number

--- fact.cpp
+++ fact.cpp
@@ -1,16 +1,44 @@
-#include<bits/stdc++.h>
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int num;
-    cin>>num;
-    for(int i=1; i*i<=num; i++){
-        if(num%i==0){
-            cout<<i<<" ";
-            if(num/i!=i){
-                cout<<num/i<<" "; //fact
+ // } Driver Code Ends
+class Solution
+{
+	public:
+    long long factorSum(int N)
+    {   
+        if(N==1){
+            return 1;
+        }
+        unsigned long int sum=0;
+        for(int i=1; i<=sqrt(N); i++){
+            if(N%i==0){
+                if(i==(N/i)){
+                    sum +=i;
+                }else
+                    sum+=(i+N/i);
             }
         }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+
+        Solution ob;
+       	cout <<  ob.factorSum(N) << "\n";
+   
     }
     return 0;
 }
---
+++ sumOfPrimes.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int primeSum(int n){
+        int reverse = 0, remainder;
+
+        while (n != 0) {
+            remainder = n % 10;
+            reverse = reverse * 10 + remainder;
+            if(reverse%)
+            n /= 10;
+        }
+        return reverse;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.primeSum(N)<<"\n";
+    }
+    return 0;
+} 
