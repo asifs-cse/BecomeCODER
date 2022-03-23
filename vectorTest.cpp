9729276810a7d22412891bebf2bf74bc284d7104
asifs-cse <asifs.cse@gmail.com>
2022-03-23 10:57:11 +0530

    vector practice

---
+++ vectorTest.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> ve;
+    ve.push_back(10);
+    ve.push_back(20);
+    ve.push_back(30);
+
+    for(int i=0;i<ve.size();i++)cout<<ve[i]<<" ";
+    ve.pop_back();
+    cout<<"\n";
+    for(auto it:ve){
+        cout<<it<<" ";
+    }
+    for(auto it=ve.rbegin();it!=ve.rend();it++){
+        cout<<*it<<" ";
+    }
+    return 0;
+
+}
