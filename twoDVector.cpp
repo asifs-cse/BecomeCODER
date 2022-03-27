d82024135c74cbefddf5576e89511336dc2e81f9
asifs-cse <asifs.cse@gmail.com>
2022-03-27 23:04:38 +0530

    create two dimonshion array

--- twoDVector.cpp
+++ twoDVector.cpp
@@ -6,11 +6,11 @@ int main(){
         {1,2,3},
         {4,5,6},
         {7,8,9}
-    }; //two d vector
+    }; //create two d vector
 
     for(int i=0; i<vect.size(); i++){
         for(int j=0;j<vect[i].size(); j++){
-            cout<<vect[i][j]<<" ";
+            cout<<vect[i][j]<<" "; //print vector value
         }
         cout<<endl;
     }
